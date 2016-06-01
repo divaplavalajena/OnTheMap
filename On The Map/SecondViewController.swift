7ddbf2b9d50958dc4261cdea053317f6c3149741
Jena Grafton <JenaLGrafton@gmail.com>
2016-06-01 14:16:11 -0500

    Initial Commit

---
+++ On The Map/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  On The Map
+//
+//  Created by Jena Grafton on 6/1/16.
+//  Copyright © 2016 Bella Voce Productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
