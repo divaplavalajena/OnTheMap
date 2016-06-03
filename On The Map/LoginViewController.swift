1a7ad3f3e9763e27cbc85b16003ba60762006a33
Jena Grafton <JenaLGrafton@gmail.com>
2016-06-03 15:29:34 -0500

    Storyboard and UI mostly complete - stilling missing cancel button on InfoPostVC second screen  - text views need more functionality - alert views still needed

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -9,18 +9,58 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet var EmailTextField: UITextField!
+    
+    @IBOutlet var PasswordTextField: UITextField!
+    
+    @IBOutlet var debugTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        debugTextLabel.text = ""
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func LoginButton(sender: AnyObject) {
+        
+        completeLogin()
+        
+        /*
+        TMDBClient.sharedInstance().authenticateWithViewController(self) { (success, errorString) in
+            performUIUpdatesOnMain {
+                if success {
+                    self.completeLogin()
+                } else {
+                    self.displayError(errorString)
+                }
+            }
+        }
+        */
     }
     
+    
+    @IBAction func SignUpButton(sender: AnyObject) {
+    }
+    
+    private func completeLogin() {
+        debugTextLabel.text = ""
+        let controller = storyboard!.instantiateViewControllerWithIdentifier("ManagerTabBarController") as! UITabBarController
+        presentViewController(controller, animated: true, completion: nil)
+    }
+
+    private func displayError(errorString: String?) {
+        if let errorString = errorString {
+            debugTextLabel.text = errorString
+        }
+    }
 
     /*
     // MARK: - Navigation
--- On The Map/TableTabViewController.swift
+++ On The Map/TableTabViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class TableTabViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    @IBAction func logoutButton(sender: AnyObject) {
+        
+        //TODO: Log Out of Udacity
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func refreshButton(sender: AnyObject) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
